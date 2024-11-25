85ab3ac45c21df797e2d71f182c02ae3e90b8424
keshavdutt <keshavdutt.1997@gmail.com>
2024-11-25 18:40:49 +0530

    added auth for other routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/write(.*)'])
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/workspace(.*)', '/notes(.*)', '/collection(.*)', '/notebook(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
